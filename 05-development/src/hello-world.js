ec09a1ddcc57395adb5f68a4c90f269c59e5d609
JobbyM <songzhipengmune@gmail.com>
2022-04-01 14:34:11 +0800

    add 05-development

---
+++ 05-development/src/hello-world.js
@@ -0,0 +1,6 @@
+function helloWorld() {
+    consoe.log('Hello world')
+}
+
+// 导出函数模块
+export default helloWorld
---
+++ 05-development/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        filename: 'bundle.js',
+
+        // 输出文件夹必须定义为绝对路径
+        path: path.resolve(__dirname, 'dist'),
+
+        // 打包前清理 dist 文件夹
+        clean: true
+    },
+
+    // 开发模式
+    mode: 'development',
+    
+    // 在开发模式下追踪代码
+    devtool: 'inline-source-map',
+
+    plugins: [
+        // 实例化 html-webpack-plugin
+        new HtmlWebpackPlugin({
+            template: './index.html', // 打包生成的文件模板
+            filename: 'app.html', // 打包生成的文件名称，默认为index.html
+            inject: 'body' // 设置所有资源文件注入模板的位置。可以设置的值 true|'head'|'body'|false, 默认为true
+            
+        })
+    ],
+
+    // dev-server
+    devServer: {
+        static: './dist'
+    }
+}
