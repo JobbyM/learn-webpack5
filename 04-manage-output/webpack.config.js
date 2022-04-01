fb47286c40d4b91fd4dd19c02931417c684f4e92
JobbyM <songzhipengmune@gmail.com>
2022-04-01 14:01:42 +0800

    add 04-manage-output

--- 04-manage-output/webpack.config.js
+++ 04-manage-output/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -7,8 +8,21 @@ module.exports = {
         filename: 'bundle.js',
 
         // 输出文件夹必须定义为绝对路径
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+
+        // 打包前清理 dist 文件夹
+        clean: true
     },
 
-    mode: 'none'
+    mode: 'none',
+
+    plugins: [
+        // 实例化 html-webpack-plugin
+        new HtmlWebpackPlugin({
+            template: './index.html', // 打包生成的文件模板
+            filename: 'app.html', // 打包生成的文件名称，默认为index.html
+            inject: 'body' // 设置所有资源文件注入模板的位置。可以设置的值 true|'head'|'body'|false, 默认为true
+            
+        })
+    ]
 }
