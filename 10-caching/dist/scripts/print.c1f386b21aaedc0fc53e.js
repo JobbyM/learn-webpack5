7b1a523adbbb261bb968b44333998f61621a0e7e
JobbyM <songzhipengmune@gmail.com>
2022-04-06 18:01:45 +0800

    add 10-caching

---
+++ 10-caching/dist/scripts/print.c1f386b21aaedc0fc53e.js
@@ -0,0 +1,21 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["print"],{
+
+/***/ "./src/print.js":
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "print": () => (/* binding */ print)
+/* harmony export */ });
+var print = function print() {
+  console.log('preload chunk.');
+};
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0cy9wcmludC5jMWYzODZiMjFhYWVkYzBmYzUzZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7O0FBQU8sSUFBTUEsS0FBSyxHQUFHLFNBQVJBLEtBQVEsR0FBTTtBQUN2QkMsRUFBQUEsT0FBTyxDQUFDQyxHQUFSLENBQVksZ0JBQVo7QUFDSCxDQUZNIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vc3JjL3ByaW50LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBwcmludCA9ICgpID0+IHtcclxuICAgIGNvbnNvbGUubG9nKCdwcmVsb2FkIGNodW5rLicpXHJcbn0iXSwibmFtZXMiOlsicHJpbnQiLCJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
